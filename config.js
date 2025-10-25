62d48d938ad9a5e2a31471b8916252c048ed5a90
masskarem1 <109003391+masskarem1@users.noreply.github.com>
2025-10-26 02:16:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const config = {
   // --- Security & API ---
   requireLogin: true, // Set to true to enable student ID login
   aiHelperProxyUrl: 'https://script.google.com/macros/s/AKfycbxzKK4RKp0rpCZcznOYPyV4aWMhBZLqYSn_ZFyNe3EO6_MxPWHZ3laF1QGL6zk6E4-h/exec',
-  loginProxyUrl: 'https://script.google.com/macros/s/AKfycbzR9crl1SITQ242nb9N4Z6El5PdOIeJha6wTRnuc2mRQJltTF83UmFfhOkftiGlBytr/exec',
+  loginProxyUrl: 'https://script.google.com/macros/s/AKfycbwp-adP5rZLVHliN-oPPscoooixXb1xGrJTccnR6w8AmDAeV8UBQrycjSPpdXaiQwjRlQ/exec',
   
   // --- Content (Chapters are now loaded from chapters.json) ---
   chapters: [], 
